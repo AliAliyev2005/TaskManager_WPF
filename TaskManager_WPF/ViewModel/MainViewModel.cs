39b410e7c0cb393b5017d948de26f7824bc4b7d2
AliAliyev2005 <ggaligg2005@gmail.com>
2021-08-19 15:02:51 +0400

    Add project files.

---
+++ TaskManager_WPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using GalaSoft.MvvmLight.Command;
+using TaskManager_WPF.View;
+
+namespace TaskManager_WPF.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Process> tasks;
+        public ObservableCollection<Process> Tasks
+        {
+            get { return tasks; }
+            set { tasks = value; OnPropertyChanged(); }
+        }
+
+        public RelayCommand<DataGrid> EndTaskCommand { get; set; }
+        public RelayCommand RunNewTaskCommand { get; set; }
+        public RelayCommand ExitCommand { get; set; }
+
+        public MainViewModel()
+        {
+            Tasks = new ObservableCollection<Process>();
+
+            GetTasks();
+
+            EndTaskCommand = new RelayCommand<DataGrid>(EndTask);
+            RunNewTaskCommand = new RelayCommand(RunNewTask);
+            ExitCommand = new RelayCommand(() => { Application.Current.Shutdown(); });
+        }
+
+        public void RunNewTask()
+        {
+            RunTask runTask = new RunTask();
+            runTask.ShowDialog();
+        }
+
+        public void EndTask(DataGrid dataGrid)
+        {
+            if (dataGrid.SelectedItem != null)
+            {
+                var process = dataGrid.SelectedItem as Process;
+                process.Kill();
+                GetTasks();
+            }
+            else MessageBox.Show("You must select a task !");
+        }
+
+        private void GetTasks()
+        {
+            Tasks.Clear();
+            foreach (Process process in Process.GetProcesses())
+            {
+                Tasks.Add(process);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ TaskManager_WPF/ViewModel/RunTaskViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using GalaSoft.MvvmLight.Command;
+
+namespace TaskManager_WPF.ViewModel
+{
+    public class RunTaskViewModel : INotifyPropertyChanged
+    {
+        private string taskName;
+        public string TaskName
+        {
+            get { return taskName; }
+            set { taskName = value; OnPropertyChanged(); }
+        }
+
+        public RelayCommand RunTaskCommand { get; set; }
+
+        public RunTaskViewModel()
+        {
+            RunTaskCommand = new RelayCommand(RunTask);
+        }
+
+        public void RunTask()
+        {
+            try
+            {
+                Process.Start(TaskName);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Process name is not correct !");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
